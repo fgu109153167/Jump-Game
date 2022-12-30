01ba2ee9e8a642986e1433961493dcc23ec334ff
fgu109153167 <118702756+fgu109153167@users.noreply.github.com>
2022-12-30 22:55:58 +0800

    初次上傳

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    Animator animator;
+    public float jumpForce = 680.0f;
+    public float walkForce = 30.0f;
+    public float maxWalkSpeed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("JumpTrigger");
+            rigid2D.AddForce(transform.up * jumpForce);
+        }
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        float speedx = Mathf.Abs(rigid2D.velocity.x);
+
+        if (speedx < maxWalkSpeed)
+        {
+            rigid2D.AddForce(transform.right * key * walkForce);
+        }
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+        if (rigid2D.velocity.y == 0)
+        {
+            animator.speed = speedx / 2.0f;
+        }
+        else
+        {
+            animator.speed = 1.0f;
+        }
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene("ClearScene");
+    }
+}
