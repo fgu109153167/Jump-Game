bd28be7fa437106df937b6ee74a9d0396f63a11b
fgu109153167 <118702756+fgu109153167@users.noreply.github.com>
2022-12-30 22:57:26 +0800

    修改問題
    修改跳躍及出界問題

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class Player : MonoBehaviour
 {
     Rigidbody2D rigid2D;
@@ -16,21 +15,20 @@ public class Player : MonoBehaviour
         rigid2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
-
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-        {
-            animator.SetTrigger("JumpTrigger");
-            rigid2D.AddForce(transform.up * jumpForce);
-        }
+            if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
+            {
+                animator.SetTrigger("JumpTrigger");
+                rigid2D.AddForce(transform.up * jumpForce);
+                this.rigid2D.AddForce(transform.up * this.jumpForce);
+            }
         int key = 0;
         if (Input.GetKey(KeyCode.RightArrow)) key = 1;
         if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
-
         float speedx = Mathf.Abs(rigid2D.velocity.x);
-
         if (speedx < maxWalkSpeed)
         {
             rigid2D.AddForce(transform.right * key * walkForce);
@@ -48,6 +46,10 @@ public class Player : MonoBehaviour
             animator.speed = 1.0f;
         }
 
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
     }
     void OnTriggerEnter2D(Collider2D other)
     {
