24ee82d5cdf9f5709e45b38970b53e1833a14fd8
fgu109153167 <118702756+fgu109153167@users.noreply.github.com>
2022-12-30 23:19:17 +0800

    完成箭頭掉落

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
+//using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
+    public GameObject ArrowPrefab;
+    public GameObject Player;
+    Vector3 playerPos;
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("MakeArrow", 0, 1.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            SceneManager.LoadScene("GameScene");
-        }
+        playerPos = Player.transform.position;
+    }
+    void MakeArrow()
+    {
+        float px = Random.Range(-3.0f, 4.0f);
+        float py = playerPos.y + 6;
+        Instantiate(ArrowPrefab, new Vector3(px, py, 0), Quaternion.identity);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,8 @@ public class Player : MonoBehaviour
 {
     Rigidbody2D rigid2D;
     Animator animator;
-    public float jumpForce = 680.0f;
-    public float walkForce = 30.0f;
+    public float jumpForce = 350.0f;
+    public float walkForce = 5.0f;
     public float maxWalkSpeed = 2.0f;
     // Start is called before the first frame update
     void Start()
@@ -18,13 +18,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
-            {
-                animator.SetTrigger("JumpTrigger");
-                rigid2D.AddForce(transform.up * jumpForce);
-                this.rigid2D.AddForce(transform.up * this.jumpForce);
-            }
+        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
         int key = 0;
         if (Input.GetKey(KeyCode.RightArrow)) key = 1;
         if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
@@ -45,14 +42,13 @@ public class Player : MonoBehaviour
         {
             animator.speed = 1.0f;
         }
-
         if (transform.position.y < -10)
         {
             SceneManager.LoadScene("GameScene");
         }
     }
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene("ClearScene");
+        //SceneManager.LoadScene("ClearScene");
     }
 }
